4bba38d33b6dba8e2ac4917a6e4dcecb636b35ea
Nikhil Agarwal <79377408+Nikhil04-india@users.noreply.github.com>
2025-08-21 22:56:47 +0530

    commit

---
+++ Animated Hero Section/script.js
@@ -0,0 +1,6 @@
+// Optionally, we can add interactive behavior
+// Example: make CTA button alert when clicked
+
+document.querySelector(".cta-btn").addEventListener("click", () => {
+  alert("🚀 You clicked Get Started!");
+});
